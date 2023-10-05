2794e2d154a19cc7ca7ee7522dd36570e09504c2
machleslie <machariales@gmail.com>
2023-10-05 15:08:23 +0300

    assignment

--- js/index.js
+++ js/index.js
@@ -1,27 +1,38 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-const url = 'https://api.github.com/search/users?q=octocat';
-    function searchName() {
-        const submit = document.querySelector('#submit');
-        submit.addEventListener('click', (e) => {
-            e.preventDefault();
-            const search = document.querySelector('#search').value;
-            fetch(`https://api.github.com/search/users?q=${search}`)
+    const userlist = document.querySelector("#user-list")
+    const submit = document.querySelector('#submit');
+    const search = document.querySelector('#search');
+    const nameInput = search.value;
+    
+    const userApiEndpoint =`https://api.github.com/search/users?q=${search}`
+    function getData(){
+        fetch(userApiEndpoint)
                 .then(response => response.json())
-                .then(data => {
-                    let user = data.items.find(user => user.login === search);
-                    if (user) {
-                        const displayName = document.createElement('ul');
-                        displayName.innerText = user.login;
-                        document.body.appendChild(displayName);
-                    } else {
-                        console.log('User not found');
-                    }
-                })
-                .catch(error => console.error('Error:', error));
-        });
+                .then(data => console.log(data) )
+                .catch(err=> console.log(`Error`,err))
     }
     
+    const  searchName = () => {
+        const send = submit.addEventListener('click',()=>{
+        const getUser =  getData();         
+        let dataDisplay = getUser.filter((event)=>{
+            if (nameInput===""){return event}
+            else if(event.name.tolowercase().includes(nameInput.tolowercase()))
+            {return event}
+        }).map((object)=>{
+            const {name, username ,avator } =object;
+            return`
+            <div class =container>
+            <p> Name: ${name}</p><br>
+            <p> username:${username}</p><br>
+            <img scr ="${avator}><br>
+            <hr>
+            </div>`
+        }).join('');
+            userlist.appendChild(dataDisplay);
+            
 
-
-searchName();
+        })
+    }
+    searchName();
 })
